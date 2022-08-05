13c3e2a92f74932d991499cb8cc61f35e20ec506
dingdenghai <sevend0324>
2022-08-05 20:09:58 +0800

    人力资源管理项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+// 页面标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
